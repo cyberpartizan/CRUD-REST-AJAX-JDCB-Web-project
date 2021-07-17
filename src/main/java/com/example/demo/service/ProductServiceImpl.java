1195c8ed5b6eb302a57c62e62b36bdb6bcea3799
karibov@btlab.ru <12345678>
2021-07-17 10:51:41 +0300

    format

--- src/main/java/com/example/demo/service/ProductServiceImpl.java
+++ src/main/java/com/example/demo/service/ProductServiceImpl.java
@@ -10,36 +10,38 @@ import com.example.demo.model.Product;
 
 @Service
 public class ProductServiceImpl implements ProductService {
- 
- @Autowired
- private ProductDaoImpl productDao;
 
- @Override
- public List<Product> getAllProducts() {
-  return productDao.getAllProducts();
- }
- public void createTable() {
-	 productDao.createTable();
- }
- @Override
- public Product findProductById(int id) {
-  return productDao.findeProductById(id);
- }
+    @Autowired
+    private ProductDaoImpl productDao;
 
- @Override
- public void addProduct(Product product) {
-  productDao.addProduct(product);
- }
+    @Override
+    public List<Product> getAllProducts() {
+        return productDao.getAllProducts();
+    }
+
+    public void createTable() {
+        productDao.createTable();
+    }
+
+    @Override
+    public Product findProductById(int id) {
+        return productDao.findeProductById(id);
+    }
+
+    @Override
+    public void addProduct(Product product) {
+        productDao.addProduct(product);
+    }
 
 
- @Override
- public void updateProduct(Product product) {
-  productDao.updateProduct(product);
- }
+    @Override
+    public void updateProduct(Product product) {
+        productDao.updateProduct(product);
+    }
 
- @Override
- public void deleteProduct(int id) {
-  productDao.deleteProduct(id);
- }
+    @Override
+    public void deleteProduct(int id) {
+        productDao.deleteProduct(id);
+    }
 
 }
